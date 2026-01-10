d3d34d060832ae9f5db12939d34436b62efd33e2
yash <yashajaymagar10@gmail.com>
2026-01-10 23:53:04 +0530

    Completed the 5th practical

---
+++ Practicals/practical 5/Qu_8.py
@@ -0,0 +1,13 @@
+# Find sum of four-digit number.
+
+num = int(input("Enter a four digit number: "))
+temp = num
+reminder = 0
+result = 0
+
+while(temp != 0):
+    reminder = temp % 10
+    result = result + reminder
+    temp = temp // 10
+
+print(f"Addition of {num} is: {result}")
