5d4e06de5107fd3ef8bb526154961da629f5559d
yash <yashajaymagar10@gmail.com>
2026-01-10 22:28:11 +0530

    Added corrected 5th question of practical 3

--- Practicals/practical 3/Qu_5.py
+++ Practicals/practical 3/Qu_5.py
@@ -1,12 +1,12 @@
 # Write a program to convert bits to Megabytes, Gigabytes and Terabytes.
 
 bits = int(input("Enter bits: "))
+bytes_ = bits / 8
+kb = bytes_ / 1024
 
-mb = bits / 1024
+mb = kb / 1024
 gb = mb / 1024
 tb = gb / 1024
-# mb = bits / 1024
-# mb = bits / 1024
 
 print(f"{bits} is equal to {mb} MB")
 print(f"{bits} is equal to {gb} GB")
