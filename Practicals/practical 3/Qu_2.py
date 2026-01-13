aa6186f193c43d1d7e9abc9c52c11d5b2aa5cb13
yash <yashajaymagar10@gmail.com>
2026-01-13 16:10:19 +0530

    .\.git\config

--- Practicals/practical 3/Qu_2.py
+++ Practicals/practical 3/Qu_2.py
@@ -1,7 +1,7 @@
 # Describe about different Bitwise operators in Python with appropriate examples. 
 
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
 
 print("a & b: ", a&b)
 print("a | b: ", a|b)
